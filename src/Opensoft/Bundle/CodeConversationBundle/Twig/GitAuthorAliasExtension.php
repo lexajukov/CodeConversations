b13f5199eb32fe4e5ec2e67e414bdbb2edc9c633
Richard Fullmer <richardfullmer@gmail.com>
2011-10-08 11:25:02 -0400

    Add a twig function for getting the gitAlias on a user, if it exists

---
+++ src/Opensoft/Bundle/CodeConversationBundle/Twig/GitAuthorAliasExtension.php
@@ -0,0 +1,69 @@
+<?php
+/*
+ *
+ */
+
+namespace Opensoft\Bundle\CodeConversationBundle\Twig;
+
+use Symfony\Bundle\DoctrineBundle\Registry;
+
+/**
+ *
+ *
+ * @author Richard Fullmer <richard.fullmer@opensoftdev.com>
+ */
+class GitAuthorAliasExtension extends \Twig_Extension
+{
+    /**
+     * @var \Doctrine\ORM\EntityManager
+     */
+    private $em;
+
+    private static $cachedUserLookup;
+
+    public function __construct(Registry $doctrineRegistry)
+    {
+        $this->em = $doctrineRegistry->getEntityManager();
+    }
+
+    public function getFunctions()
+    {
+        return array(
+            'git_author' => new \Twig_Function_Method($this, 'getGitAuthor'),
+            'git_author_exists' => new \Twig_Function_Method($this, 'exists'),
+        );
+    }
+
+    public function exists($authorName)
+    {
+        if (!isset(self::$cachedUserLookup[$authorName])) {
+            self::$cachedUserLookup[$authorName] = $this->em->getRepository('OpensoftCodeConversationBundle:User')->findOneBy(array('gitAlias' => $authorName));
+//            print_r(" checking exists");
+        }
+        
+//        print_r(self::$cachedUserLookup);
+//        die();
+
+        return null !== self::$cachedUserLookup[$authorName];
+    }
+
+    public function getGitAuthor($authorName)
+    {
+        if (!isset(self::$cachedUserLookup[$authorName])) {
+            self::$cachedUserLookup[$authorName] = $this->em->getRepository('OpensoftCodeConversationBundle:User')->findOneBy(array('gitAlias' => $authorName));
+        } 
+        
+        return self::$cachedUserLookup[$authorName];
+    }
+
+    /**
+     * Returns the name of the extension.
+     *
+     * @return string The extension name
+     */
+    function getName()
+    {
+        return 'git_author_alias';
+    }
+
+}
