d2789ef84f2545ba633c37b171133532caec9d5a
Richard Fullmer <richardfullmer@gmail.com>
2011-10-07 20:08:52 -0400

    Add gitAlias to user, add alerts as session messages, and style some default FOS forms

---
+++ src/Opensoft/Bundle/CodeConversationBundle/Form/Type/ProfileFormType.php
@@ -0,0 +1,48 @@
+<?php
+/*
+ * This file is part of ProFIT
+ *
+ * Copyright (c) 2011 Farheap Solutions (http://www.farheap.com)
+ *
+ * The unauthorized use of this code outside the boundaries of
+ * Farheap Solutions Inc. is prohibited.
+ */
+
+namespace Opensoft\Bundle\CodeConversationBundle\Form\Type;
+
+use Symfony\Component\Form\FormBuilder;
+use FOS\UserBundle\Form\Type\ProfileFormType as BaseType;
+
+/**
+ *
+ *
+ * @author Richard Fullmer <richard.fullmer@opensoftdev.com>
+ */ 
+class ProfileFormType extends BaseType
+{
+    /**
+     * Returns the name of this type.
+     *
+     * @return string The name of this type
+     */
+    function getName()
+    {
+        return 'opensoft_codeconversation_profile';
+    }
+
+
+    /**
+     * Builds the embedded form representing the user.
+     *
+     * @param FormBuilder $builder
+     * @param array $options
+     */
+    protected function buildUserForm(FormBuilder $builder, array $options)
+    {
+        $builder
+            ->add('username')
+            ->add('email', 'email')
+            ->add('gitAlias')
+        ;
+    }
+}
