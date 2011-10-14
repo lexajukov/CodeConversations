8d0bd1084401e46112c639176704bb920e0f76fb
Richard Fullmer <richardfullmer@gmail.com>
2011-10-14 11:26:27 -0400

    Fix diff combined detection of modified files

--- src/Opensoft/Bundle/CodeConversationBundle/Git/Repository.php
+++ src/Opensoft/Bundle/CodeConversationBundle/Git/Repository.php
@@ -8,10 +8,8 @@ namespace Opensoft\Bundle\CodeConversationBundle\Git;
 use PHPGit_Repository;
 use Opensoft\Bundle\CodeConversationBundle\Model\ProjectInterface;
 use Opensoft\Bundle\CodeConversationBundle\Model\RemoteInterface;
-use Opensoft\Bundle\CodeConversationBundle\Model\Diff;
-use Opensoft\Bundle\CodeConversationBundle\Model\FileDiff;
-use Opensoft\Bundle\CodeConversationBundle\Model\FileDiffChunk;
 use Opensoft\Bundle\CodeConversationBundle\Model\Commit;
+use Opensoft\Bundle\CodeConversationBundle\Git\Diff\DiffHeaderParser;
 
 /**
  *
@@ -147,7 +145,7 @@ class Repository
         $commit->setMessage($message . "\n" . $subMessage);
 
         if (isset($output[$i])) {
-            $diff = $this->parseDiffOutput(array_slice($output, $i));
+            $diff = DiffHeaderParser::parse(array_slice($output, $i));
             $commit->setDiff($diff);
         }
 
@@ -165,9 +163,7 @@ class Repository
             $parameters['%path%'] = $path;
         }
 
-        $output = explode("\n", $this->repo->git(strtr($command, $parameters)));
-        
-        return $this->parseDiffOutput($output);
+        return DiffHeaderParser::parse($this->repo->git(strtr($command, $parameters)));
     }
 
     public function getMergeBase($object1, $object2)
@@ -217,163 +213,7 @@ class Repository
     }
 
 
-    /**
-     * @param array 
-     * @return \Opensoft\Bundle\CodeConversationBundle\Model\Diff
-     */
-    private function parseDiffOutput(array $output)
-    {
-        $diffFile = null;
-        $diffChunk = null;
-        $diffChunkContent = array();
 
-        $fileDiffs = array();
-
-        $i = 0;
-        do {
-            $line = $output[$i++];
-
-            // start a new Diff object
-            if (strpos($line, 'diff --git ') === 0 || strpos($line, 'diff --cc') === 0) {
-//                print_r($i . "\n");
-
-                // Clean up old diff object, if there is one
-                if (null !== $diffFile) {
-                    if (null !== $diffChunk) {
-                        if (!empty($diffChunkContent)) {
-                            $diffChunk->setContent($diffChunkContent);
-                        }
-
-                        $diffFile->addFileDiffChunk($diffChunk);
-                        $diffChunk = null;
-                    }
-
-//                    $commit->addFileDiff($diff);
-                    $fileDiffs[] = $diffFile;
-                }
-
-                $diffFile = new FileDiff();
-
-                // COMMENTED OUT LINES BREAK diff --cc (combined diff)
-//                list($srcFileName, $dstFileName) = explode(" ", trim(substr($line, 11)));
-//                $diffFile->setSrcPath(substr($srcFileName, 2));
-//                $diffFile->setDstPath(substr($dstFileName,2));
-
-//                // Parse extended header lines
-                do {
-                    $line = $output[$i++];
-
-                    if (strpos($line, 'old mode ') === 0) {
-                        $diffFile->setSrcMode(substr($line, 8));
-                        $diffFile->setStatus(FileDiff::STATUS_MODIFICATION);
-                    } elseif (strpos($line, 'new mode ') === 0) {
-                        $diffFile->setDstMode(substr($line, 8));
-                        $diffFile->setStatus(FileDiff::STATUS_MODIFICATION);
-                    } elseif (strpos($line, 'deleted file mode') === 0) {
-                        $diffFile->setDstMode(substr($line, 18));
-                        $diffFile->setStatus(FileDiff::STATUS_DELETION);
-                    } elseif (strpos($line, 'new file mode ') === 0) {
-                        $diffFile->setDstMode(substr($line, 14));
-                        $diffFile->setStatus(FileDiff::STATUS_ADDITION);
-                    } elseif (strpos($line, 'copy from ') === 0) {
-                        $diffFile->setSrcPath(substr($line, 10));
-                        $diffFile->setStatus(FileDiff::STATUS_COPY);
-                    } elseif (strpos($line, 'copy to ') === 0) {
-                        $diffFile->setDstPath(substr($line, 8));
-                        $diffFile->setStatus(FileDiff::STATUS_COPY);
-                    } elseif (strpos($line, 'rename from ') === 0) {
-                        $diffFile->setSrcPath(substr($line, 12));
-                        $diffFile->setStatus(FileDiff::STATUS_RENAMING);
-                    } elseif (strpos($line, 'rename to ') === 0) {
-                        $diffFile->setDstPath(substr($line, 10));
-                        $diffFile->setStatus(FileDiff::STATUS_RENAMING);
-                    } elseif (strpos($line, 'index ') === 0) {
-                        if (strpos(substr($line, 6), ' ') > 0) {
-                            list($hash, $mode) = explode(" ", substr($line, 6));
-                            $diffFile->setSrcMode($mode);
-                            $diffFile->setDstMode($mode);
-                            $diffFile->setStatus(FileDiff::STATUS_MODIFICATION);
-                        } else {
-                            $hash = substr($line, 6);
-                        }
-                        list($srcHash, $dstHash) = explode("..", $hash);
-
-                        $diffFile->setSrcSha1($srcHash);
-                        $diffFile->setDstSha1($dstHash);
-                    }
-                } while ($i < count($output) && strpos($output[$i], '---') !== 0);
-//
-//                 Parse from-file/to-file header
-                if ($i < count($output)) {
-                    do {
-                        $line = $output[$i++];
-
-                        if (strpos($line, '--- ') === 0) {
-                            $diffFile->setSrcPath(substr($line, 6));
-                        } elseif (strpos($line, '+++ ') === 0 ) {
-                            $diffFile->setDstPath(substr($line, 6));
-                        }
-                    } while ($i < count($output) && strpos($output[$i], '@@') !== 0);
-                }
-            }
-
-            // Parse for diff chunk header
-//            print_r($line."\n");
-            if (strpos($line, '@@') === 0) {
-//                print_r("**** DIFF CHUNK DETECTED *****\n");
-                if (null !== $diffChunk) {
-                    if (!empty($diffChunkContent)) {
-                        $diffChunk->setContent($diffChunkContent);
-                    }
-                    $diffFile->addFileDiffChunk($diffChunk);
-                }
-
-                $diffChunk = new FileDiffChunk();
-                $diffChunk->setDescription(trim($line));
-                $diffChunkContent = array();
-
-                $diffChunkHeader = trim(str_replace(array('@@ ', ' @@'), '', substr($line, 0, strpos($line, ' @@'))));
-                $array = explode(" ", $diffChunkHeader);
-                $src = explode(",", substr($array[0], 1));
-                $dst = explode(",", substr($array[1], 1));
-
-                $diffChunk->setSrcStartLineNumber($src[0]);
-                $diffChunk->setDstStartLineNumber($dst[0]);
-            } else {
-                if (null !== $diffChunk && strpos($line, '+') === 0) {
-                    $diffChunk->incrementInsertions();
-                } elseif (null !== $diffChunk && strpos($line, '-') === 0) {
-                    $diffChunk->incrementDeletions();
-                }
-
-                $diffChunkContent[] = $line;
-            }
-
-//            $file[] = $line;
-
-
-
-//            $i++;
-        } while ($i < count($output));
-
-        if (null !== $diffFile) {
-            if (null !== $diffChunk) {
-                if (!empty($diffChunkContent)) {
-                    $diffChunk->setContent($diffChunkContent);
-                }
-
-                $diffFile->addFileDiffChunk($diffChunk);
-            }
-
-//            $commit->addFileDiff($diff);
-            $fileDiffs[] = $diffFile;
-        }
-
-        $diff = new Diff();
-        $diff->setFileDiffs($fileDiffs);
-
-        return $diff;
-    }
 
     /**
      * @param \Opensoft\Bundle\CodeConversationBundle\Model\ProjectInterface $project
